26b3b76a14b3848a5bf60003dfeaf62af65bd1c9
linpeng109 <linpeng109@aliyun.com>
2016-04-12 12:24:48 +0800

    add baseauth module

--- meeting.js
+++ meeting.js
@@ -1,17 +1,21 @@
+//引入express组件
 var express = require("express");
 var app = express();
+
+//引入安全组件 basicauth-middleware  https://www.npmjs.com/package/basicauth-middleware
+var basicauth = require("basicauth-middleware")
 
 //引入数据库组件
 var mongoose = require("mongoose")
 
-//建立数据库连接
+//建立数据库连接组件
 mongoose.connect("mongodb://root:admin@ds021989.mlab.com:21989/mongodb_test_20160324")
 var db = mongoose.connection;
 
 //加入数据库监听
 db.on('error', console.error.bind(console, '连接错误:'));
 db.once('open', function () {
-    console.log("open");
+    console.log("The mongoodb has be open");
 });
 
 //数据模型
@@ -23,10 +27,22 @@ var UserSchema = new mongoose.Schema({
 //实体类（对应数据表）
 var User = mongoose.model('userBase', UserSchema)
 
+
+//过滤器
+app.use(function (req, res, next) {
+    var headers = req.headers;
+    console.log(JSON.stringify(headers))
+    next();
+})
 //跟路径
 app.get("/", function (req, res) {
     res.send("Hello world ！");
 });
+
+//安全验证
+app.use(basicauth(function (username, password) {
+    return (username === 'abcd' && password === 'abcd')
+}))
 
 //插入数据接口
 app.get('/insert', function (req, res) {
@@ -49,7 +65,7 @@ app.get('/findByList', function (req, res) {
     var pageSize = 10;
     var pageNum = 1;
     var query = User.find({})
-    //注意分页
+    //分页
     query.limit(pageSize)
     query.skip(pageNum * pageSize)
     //排序
@@ -65,7 +81,6 @@ app.get('/findByList', function (req, res) {
         res.send(result)
     })
 });
-
 
 
 var server = app.listen(3000, function () {
